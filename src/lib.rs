b50aac1b3e2fc39bd560b1f0570f73f24b9f2d81
Max <max@recoset.com>
2017-04-17 20:46:02 -0400

    base64 now correctly expects even-numbered hex

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,7 @@ pub const HEX: [char; 16] = [
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
 ];
 
-pub fn hex_to_base64(hex: &str) -> String {
-	let mut output = String::new();
-	
+fn add_triplet<'a>(output: &'a mut String, byte0: u8, byte1: u8, byte2: u8) -> &'a mut String {
 	let digit = |byte| {
 		// println!("converting {:08b} ({})", byte, byte);
 		let dig = match byte {
@@ -24,36 +22,43 @@ pub fn hex_to_base64(hex: &str) -> String {
 		dig
 	};
 
-	for bytes in hex.as_bytes().chunks(3) {
-		if bytes.len() == 3 {
-			// println!("\nconverting '{}{}{}'", bytes[0] as char, bytes[1] as char, bytes[2] as char);
-			// println!("bytes are {:04b}.{:04b}.{:04b}", digit(bytes[0]), digit(bytes[1]), digit(bytes[2]));
+	// println!("\nconverting '{}{}{}'", byte0 as char, byte1 as char, byte2 as char);
+	// println!("bytes are {:04b}.{:04b}.{:04b}", digit(byte0), digit(byte1), digit(byte2));
 
-		    let idx1 = (digit(bytes[0]) << 2) | (digit(bytes[1]) >> 2);
-			// println!("idx1 is {:06b} ({})\n", idx1, idx1);
-            output.push(BASE_64[idx1]);
+	let idx1 = (digit(byte0) << 2) | (digit(byte1) >> 2);
+	// println!("idx1 is {:06b} ({})\n", idx1, idx1);
+	output.push(BASE_64[idx1]);
 
-			let idx2 = ((digit(bytes[1]) & 0b0011) << 4) | digit(bytes[2]);
-			// println!("idx2 is {:06b} ({})\n", idx2, idx2);
-            output.push(BASE_64[idx2]);
+	let idx2 = ((digit(byte1) & 0b0011) << 4) | digit(byte2);
+	// println!("idx2 is {:06b} ({})\n", idx2, idx2);
+	output.push(BASE_64[idx2]);
+
+	output
+}
+
+pub fn hex_to_base64(hex: &str) -> String {
+	let mut output = &mut String::new();
+	
+	for bytes in hex.as_bytes().chunks(6) {
+
+		if bytes.len() == 6 {
+			add_triplet(output, bytes[0], bytes[1], bytes[2]);
+			add_triplet(output, bytes[3], bytes[4], bytes[5]);
+		} else if bytes.len() == 4 {
+			add_triplet(output, bytes[0], bytes[1], bytes[2]);
+			add_triplet(output, bytes[3], 48, 48);
+			output.push('=');
 		} else if bytes.len() == 2 {
-		    let idx1 = (digit(bytes[0]) << 2) | (digit(bytes[1]) >> 2);
-			// println!("idx1 is {}", idx1);
-            output.push(BASE_64[idx1]);
-
-			let idx2 = digit(bytes[1] & 0xFF) << 4;
-			// println!("idx2 is {}", idx2);
-            output.push(BASE_64[idx2]);
+			add_triplet(output, bytes[0], bytes[1], 48);
+			output.push('=');
 			output.push('=');
 		} else {
-		    output.push(BASE_64[digit(bytes[0]) << 2]);
-			output.push('=');
-			output.push('=');
+			panic!("Got an odd numbered hex");
 		}
 		println!("output is {}", output);
 	}
 	
-	return output;
+	return output.clone();
 }
 
 #[cfg(test)]
@@ -69,5 +74,9 @@ mod tests {
 		let input2 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 		let output2 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 		assert!(hex_to_base64(input2) == output2);
+
+		let input3 = "12342412";
+		let output3 = "EjQkEg==";
+		assert!(hex_to_base64(input3) == output3);
 	}
 }
